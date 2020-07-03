0fb9c2bea79c07f08cc0a86d12c0a3b21b58f0b9
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-03 12:13:11 -0300

    Add associations to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   	validates :email, presence: true, uniqueness: true
   	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   	validates :username, presence: true, uniqueness: true
-  	validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+	validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+	  
+	has_many :houses, foreign_key: :customer_id
+	has_many :guard_employees, foreign_key: :employee_id
 end
