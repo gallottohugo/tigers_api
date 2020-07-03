21a062f6fa7ae6367c9594eca4df322617efe8d7
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-03 12:12:09 -0300

    add exp token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,8 +6,8 @@ class AuthenticationController < ApplicationController
     	@user = User.find_by_email(params[:email])
 		if @user&.authenticate(params[:password])
       		token = JsonWebToken.encode(user_id: @user.id)
-      		time = Time.now + 24.hours.to_i
-      		render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"), username: @user.username }, status: :ok
+      		time = Time.now + 2.years.to_i
+      		render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"), username: @user.id }, status: :ok
     	else
       		render json: { error: 'unauthorized' }, status: :unauthorized
     	end
