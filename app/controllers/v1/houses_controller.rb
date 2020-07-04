26c04ae2707be60ba221ec89c5fb2c5f6dabee60
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-04 12:13:28 -0300

    Refact

--- app/controllers/v1/houses_controller.rb
+++ app/controllers/v1/houses_controller.rb
@@ -1,29 +1,27 @@
 class V1::HousesController < ApplicationController
 	before_action :authorize_request
-	before_action :valid_customer, :valid_disctrict, only: :create
+	before_action :coordinator_permissions, :valid_customer, :valid_disctrict, only: :create
 	before_action :find_house, only: :show
 	before_action :get_houses, only: :index
 	  
 
 	# POST /houses
 	def create
-		if @current_user.admin
-			@house = House.new(house_params)
-			if @house.save
-				render json: @house, status: :created
-			else
-				render json: { errors: @house.errors.full_messages }, status: :unprocessable_entity
-			end
+		@house = House.new(house_params)
+		if @house.save
+			render json: @house, status: :created
 		else
-			render json: { errors: 'You are not admin' }, status: :unauthorized
+			render json: { errors: @house.errors.full_messages }, status: :unprocessable_entity
 		end
 	end
+
 
 	# GET /houses
 	def index
     	render json: @houses, status: :ok
 	end
 	  
+
 	# GET /houses/:id
   	def show
     	render json: @house, status: :ok
@@ -36,17 +34,20 @@ class V1::HousesController < ApplicationController
     	params.permit(:name, :address, :address_number, :city, :customer_id, :district_id)
 	end
 
+
 	def valid_disctrict
 		@district = District.find(params[:district_id])
     	rescue ActiveRecord::RecordNotFound
       		render json: { errors: 'District not found' }, status: :not_found
 	end
 
+
 	def valid_customer
 		@customer = User.find(params[:customer_id])
     	rescue ActiveRecord::RecordNotFound
       		render json: { errors: 'Customer not found' }, status: :not_found
 	end
+
 
 	def find_house
 		@house = House.find(params[:id])
