20a232dd3115ff1b53d2397e8b24e3e1bd8ee420
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-04 12:13:21 -0300

    Refact

--- app/controllers/v1/districts_controller.rb
+++ app/controllers/v1/districts_controller.rb
@@ -1,21 +1,38 @@
 class V1::DistrictsController < ApplicationController
 	before_action :authorize_request
-	#before_action :employee_permissions, only: :houses
-	before_action :find_district, only: [ :houses, :show ]
+	before_action :coordinator_permissions, only: :create
+	before_action :employee_permissions, only: :index
+	before_action :find_district, only: :show
 
-
-	def houses
-		render json: @district.houses, status: :ok
-	end
 
 	# GET /districts/:id
 	def show
     	render json: @district, status: :ok
 	end
+
+
+	def create
+		@distric = District.new(distric_params)
+		if @distric.save
+			render json: @distric, status: :created
+		else
+			render json: { errors: @distric.errors.full_messages }, status: :unprocessable_entity
+		end
+	end
+
+
+	def index
+		render json: District.all, status: :ok
+	end
 	  
 
 	private 
 
+	def distric_params
+    	params.permit(:name)
+	end
+
+	
 	def find_district
 		@district = District.find(params[:id])
     	rescue ActiveRecord::RecordNotFound
