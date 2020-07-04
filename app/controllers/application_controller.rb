09fef931ff625bd3afb8f8639032124e786d3294
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-04 11:28:42 -0300

    Create permissions to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
 	def not_found
-		render json: { error: 'not_found' }
+		render json: { error: 'Page not found ' }
 	end
 	
 	def authorize_request
@@ -13,6 +13,25 @@ class ApplicationController < ActionController::API
 		  	render json: { errors: e.message }, status: :unauthorized
 		rescue JWT::DecodeError => e
 		  	render json: { errors: e.message }, status: :unauthorized
+		end
+	end
+
+
+	def admin_permissions
+		unless ['admin'].include? @current_user.user_type 
+			render json: { errors: 'Unauthorized - You are not admin' }, status: :unauthorized
+		end
+	end
+
+	def coordinator_permissions
+		unless ['admin', 'coordinator'].include? @current_user.user_type
+			render json: { errors: 'Unauthorized - You are not a coordinator' }, status: :unauthorized
+		end
+	end
+
+	def employee_permissions
+		unless ['admin', 'coordinator', 'employee'].include? @current_user.user_type
+			render json: { errors: 'Unauthorized - You are not a emloyee' }, status: :unauthorized
 		end
 	end
 end
