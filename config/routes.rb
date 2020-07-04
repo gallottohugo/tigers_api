50f3479ec26ff6b00a3c9563295561261dc17f87
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-04 11:30:55 -0300

    Refact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  	# devise_for :users
+  	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  	#get '/*a', to: 'application#not_found'
+  	namespace :v1 do 
+    	resources :users
+    	resources :houses
+    	resources :districts do
+      		resources :houses, only: :index
+		end
+    	post '/auth/login', to: 'authentication#login'
+    	#get '/*a', to: 'application#not_found'  
+	end
 
 
-  resources :users
-  post '/auth/login', to: 'authentication#login'
-  get '/*a', to: 'application#not_found'
-
-
-  #namespace :v1 do
-    #resources :sessions, only: [:create, :destroy]
-    #resources :users, only: [ :create, :destroy ]
-  #end 
+  	#namespace :v1 do
+    	#resources :sessions, only: [:create, :destroy]
+    	#resources :users, only: [ :create, :destroy ]
+  	#end 
 end
